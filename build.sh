c39a90d8fbbaefb2bccaa326b636b639502a905f
Krisssss532 <ashwiniprasad@live.com.au>
2026-06-28 18:52:22 +1000

    Update build.sh to account for 6rrp

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ mkdir -p dist/w5p/3.05.00
 mkdir -p dist/wm6/1.03.04
 mkdir -p dist/w6r/2.00.08
 mkdir -p dist/w6w/3.05.03
+mkdir -p dist/w6p/4.00.13
 
 # Copy DLL
 cp build/bngrw_wm5_10500.dll dist/wm5/1.05.00/bngrw.dll
@@ -23,7 +24,7 @@ cp build/bngrw_w5p_30500.dll dist/w5p/3.05.00/bngrw.dll
 cp build/bngrw_wm6_10304.dll dist/wm6/1.03.04/bngrw.dll
 cp build/bngrw_w6r_20008.dll dist/w6r/2.00.08/bngrw.dll
 cp build/bngrw_w6w_30503.dll dist/w6w/3.05.03/bngrw.dll
-
+cp build/bngrw_w6p_40013.dll dist/w6p/4.00.13/bngrw.dll
 echo "Build complete!"
 echo "  dist/wm5/1.05.00/bngrw.dll"
 echo "  dist/w5x/2.00.02/bngrw.dll"
@@ -31,3 +32,4 @@ echo "  dist/w5p/3.05.00/bngrw.dll"
 echo "  dist/wm6/1.03.04/bngrw.dll"
 echo "  dist/w6r/2.00.08/bngrw.dll"
 echo "  dist/w6w/3.05.03/bngrw.dll"
+echo "  dist/w6p/4.00.13/bngrw.dll"
